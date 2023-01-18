085c3f82959b0f851db7f99dc647599d68c9d2bd
Gust4v0M <gustavomorgado6@gmail.com>
2023-01-17 23:29:14 -0300

    try make del in js

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
 let lista = document.getElementById("lista");
 let bnt = document.getElementById("bnt");
 let input = document.getElementById("write");
+let check = document.getElementById("check");
 
 
 
 onload = function (){
 
   let nome = JSON.parse(localStorage.getItem("tarefas" ) || [] );
-  lista.innerHTML += `<li>
+
+
+  lista.innerHTML += ` <li>
                       ${nome}
-                    </li>`;
+                    </li>
+                    <br>`;
 
 console.log(nome)
 }
 
 bnt.addEventListener("click", function () {
-  let array = [];
-  let valor = input.value;
+  var array = [];
+  var valor = input.value;
  
   array.push([valor]);
 
  localStorage.setItem("tarefas", JSON.stringify(array))     
 
 
- lista.innerHTML += `<li>
+ lista.innerHTML += `<input type="checkbox"> <li>
                      ${array}
-                   </li>`;
+                   </li>
+                   <br>`;
+
+
+        
 
 
  console.log(array)
-    });
+      });
+
+      lista.addEventListener("click", function(){
+      let del = document.createElement('del')
+      
+        del.appendChild(lista)
 
 
 
-
-
+        console.log("vamos ver".tagDel)
+      })
+      
 
 
